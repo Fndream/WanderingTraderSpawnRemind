86733ad33406a4125a14d4f97a5289a0c95b7abf
Fndream <2835322603@qq.com>
2023-02-04 20:53:08 +0800

    v1.0

---
+++ src/main/java/com/github/fndream/wanderingtraderspawnremind/Main.java
@@ -0,0 +1,9 @@
+package com.github.fndream.wanderingtraderspawnremind;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
